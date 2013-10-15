3e12b557b2b5ead307182104d55174f93dd89720
imsamurai <im.samuray@gmail.com>
2013-10-15 13:49:34 +0300

    add to travis ci

---
+++ Test/Case/AllArraySortTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class AllArraySortTest extends PHPUnit_Framework_TestSuite {
+
+	/**
+	 * Suite define the tests for this suite
+	 *
+	 * @return void
+	 */
+	public static function suite() {
+		$suite = new CakeTestSuite('All ArraySort Tests');
+
+		$path = App::pluginPath('ArraySort') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+
+}
